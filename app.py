4b582b256541971b51731725aebb2444f0905951
Breno Robert <brenorobert@ucl.br>
2021-10-20 18:31:04 -0300

    QUASE PRONTO :+1:

--- app.py
+++ app.py
@@ -15,21 +15,34 @@ def hello_world():  # put application's code here
     return render_template('app.html', texto=livro)
 """
 
+
 @app.route('/', methods=["GET", "POST"])
 def Index():
-    requisicao = requests.request('GET', 'https://api.fbi.gov/wanted/v1/list')
-    requisicao = json.loads(requisicao.content)
-    # print(requisicao)
-    return render_template('Principal.html', todosDados=requisicao, procurados=requisicao['items'])
+    return render_template('Principal.html')
+
 
 @app.route('/casos', methods=["GET", "POST"])
 def casos():
-    return render_template('casos.html')
+    requisicao = requests.request('GET', 'https://api.fbi.gov/wanted/v1/list', params = {
+            'page': '2'
+        })
+    requisicao = json.loads(requisicao.content)
+    # print(requisicao)
+    return render_template('casos.html', todosDados=requisicao, procurados=requisicao['items'])
 
 
-@app.route('/casos/<titulo_caso>', methods=["GET"])
+@app.route('/detalhes/', methods=["GET", "POST"])
 def detalhes():
-    return render_template('detalhes.html')
-    
+    pega = None
+    if request.args.get('sobre'):
+        title = request.args.get('sobre')
+        pega = requests.get('https://api.fbi.gov/wanted/v1/list', params = {
+            'title': title
+        }).json()
+    proibidinhos = []
+    campos = [{'mostrar': campo.replace('_', ' '), 'filtro': campo} for campo in pega['items'][0] if pega['items'][0][campo] and campo not in proibidinhos]
+    return render_template('detalhes.html', pessoa=pega, campos=campos)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
