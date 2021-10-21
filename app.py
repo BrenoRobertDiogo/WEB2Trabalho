0a90a1462b7086a53b70db28e218a912b4b36560
Breno Robert <brenorobert@ucl.br>
2021-10-20 23:25:39 -0300

    Alterações consideráveis e adimiráveis :ant:

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect
 import requests
 from flask.helpers import url_for
 import json
+from jinja2 import pass_eval_context
+from markupsafe import Markup, escape
+from datetime import datetime
 
 # python app.py --flask -run
 # base: https://www.fbi.gov/wanted/api
@@ -39,9 +42,9 @@ def detalhes():
         pega = requests.get('https://api.fbi.gov/wanted/v1/list', params = {
             'title': title
         }).json()
-    proibidinhos = []
+    proibidinhos = ['images', '@id', 'path', 'title', 'files', 'uid']
     campos = [{'mostrar': campo.replace('_', ' '), 'filtro': campo} for campo in pega['items'][0] if pega['items'][0][campo] and campo not in proibidinhos]
-    return render_template('detalhes.html', pessoa=pega, campos=campos)
+    return render_template('detalhes.html', pessoa=pega, campos=campos, imagem=pega['items'][0]['images'])
 
 
 if __name__ == '__main__':
